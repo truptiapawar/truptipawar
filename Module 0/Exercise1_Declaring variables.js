8523c8b20cc3ec4f3955dc1aff2f817a597ec027
truptiapawar <trupti.gharat@wbd.com>
2024-10-21 16:34:11 +0530

    update Exercise1_Declaring variables

--- Module 0/Exercise1_Declaring variables.js
+++ Module 0/Exercise1_Declaring variables.js
@@ -1,27 +1,16 @@
 /*
 Tasks
 Declare a new variable named petDog and give it the name Rex.
-
 Declare a new variable named petCat and give it the name Pepper.
-
 Console.log the petDog variable.
-
 Console.log the petCat variable.
-
 Console.log the text "My pet dog's name is: " and the petDog variable.
-
 Console.log the text "My pet cat's name is: " and the petCat variable.
-
 Declare another variable and name it catSound. Assign the string of "purr" to it.
-
 Declare another variable and name it dogSound. Assign the string of "woof" to it.
-
 Console.log the variable petDog, then the string "says", then the variable dogSound.
-
 Console.log the variable petCat, then the string "says", then the variable catSound.
-
 Reassign the value stored in catSound to the string "meow".
-
 Console.log the variable petCat, then the string "now says", then the variable catSound.
 */
 
