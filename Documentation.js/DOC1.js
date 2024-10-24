50286bf57dddf4faac80d1b23a9784f1a8ddd093
truptiapawar <trupti.gharat@wbd.com>
2024-10-24 12:56:54 +0530

    DOC1.js

---
+++ Documentation.js/DOC1.js
@@ -0,0 +1,13 @@
+/*
+Datatypes: 
+string, number, Boolean, null, undefined, BigInt and symbol
+
+Operators:
+arithmetic +,-, *, /
+Comparison < > ==  !=
+logical operator  &&  ||  !()
+
+The strict equality operator (===)which is three equal signs is used to check for value and type.
+*/
+
+
