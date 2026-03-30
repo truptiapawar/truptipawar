f32f9a5ece47f05f9b8c1230adf20b5052b32231
truptiapawar <trupti.gharat@wbd.com>
2026-03-30 14:11:29 +0530

    sample

--- Module 1 - Introduction to JS & Declaring variables/Hello.js
+++ Module 1 - Introduction to JS & Declaring variables/Hello.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+let X = 0;
+let Y = 1;
+let Z = Boolean (Number(X) > Number(Y)) ;
+
+let a = parseFloat (X) + Number (Y);
+
+console.log(Z);
+//console.log(Z);
---
+++ Module 1 - Introduction to JS & Declaring variables/try.js
@@ -0,0 +1,9 @@
+var x=8;
+if (x==8)
+{
+    let y = 10;
+} else
+{
+    let y=20;
+}
+console.log(x);
