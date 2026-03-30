a6de718ac7dd2af045acc1883d85762a734db1df
truptiapawar <trupti.gharat@wbd.com>
2026-03-30 15:29:03 +0530

    Create a addition method with two parameter and it return addition value. Print the value in console

---
+++ Module 4 & 6 - Equality Operators & Expression+Operators/Exercise7_programpractice.js
@@ -0,0 +1,9 @@
+//Create a addition method with two parameter and it return addition value. Print the value in console
+
+function addition(a,b)
+{
+let add=a+b;
+return add;
+}
+
+console.log ("Result for addition of 2 parameters =", addition(3,4));
